133f1165013f37e335bf839fcebb1571b2126863
sidz1417 <sidz1408@gmail.com>
2020-10-20 12:41:59 +0530

    optimized for cold starts

--- functions/index.js
+++ functions/index.js
@@ -1,14 +1,18 @@
-const admin = require('firebase-admin');
-const functions = require('firebase-functions');
-const { HttpsError } = require('firebase-functions/lib/providers/https');
+const functions = require('firebase-functions')
 
-admin.initializeApp()
+let is_createClaims_initialized = false
+let is_revokeClaims_initialized = false
+let is_createCategory_initialized = false
+let is_removeCategory_initialized = false
+let is_upVote_initialized = false
 
-exports.onCreateUser = functions.auth.user().onCreate((user, _) => {
-    console.log(`New user created : ${user.uid}`)
-})
 
 exports.createClaims = functions.firestore.document('moderators/{moderator}').onCreate(async (snap, _) => {
+    const admin = require('firebase-admin')
+    if (!is_createClaims_initialized) {
+        admin.initializeApp()
+        is_createClaims_initialized = true
+    }
     const userID = snap.data().uid
     try {
         let userRecord = await admin.auth().getUser(userID)
@@ -41,6 +45,11 @@ exports.createClaims = functions.firestore.document('moderators/{moderator}').on
 })
 
 exports.revokeClaims = functions.firestore.document('moderators/{userId}').onDelete(async (snap, _) => {
+    const admin = require('firebase-admin')
+    if (!is_revokeClaims_initialized) {
+        admin.initializeApp()
+        is_revokeClaims_initialized = true
+    }
     try {
         const userID = snap.data().uid
         let userRecord = await admin.auth().getUser(userID)
@@ -64,16 +73,21 @@ exports.revokeClaims = functions.firestore.document('moderators/{userId}').onDel
 
 
 exports.createCategory = functions.https.onCall(async (params, context) => {
-    if (context.auth == null) throw new HttpsError('permission-denied', 'User is not Authenticated!!')
+    const admin = require('firebase-admin')
+    if (!is_createCategory_initialized) {
+        admin.initializeApp()
+        is_createCategory_initialized = true
+    }
+    if (context.auth == null) throw new functions.https.HttpsError('permission-denied', 'User is not Authenticated!!')
     const userID = context.auth.uid
     console.log(`userID ${userID} invoked createCategory`)
     const categoryName = params.categoryName
-    if (categoryName.length > 10) throw new HttpsError('permission-denied', 'Category Name length should be less than 10 characters')
+    if (categoryName.length > 10) throw new functions.https.HttpsError('permission-denied', 'Category Name length should be less than 10 characters')
     const userRecord = await admin.auth().getUser(userID)
     const customClaims = userRecord.customClaims
     console.log(`Custom claims of user ${userID} : ${JSON.stringify(customClaims)}`)
     if (customClaims == null || !customClaims['isModerator']) {
-        throw new HttpsError('permission-denied', 'Creation not possible as user is not a moderator')
+        throw new functions.https.HttpsError('permission-denied', 'Creation not possible as user is not a moderator')
     }
     const db = admin.firestore()
     return await db.runTransaction(async t => {
@@ -83,7 +97,12 @@ exports.createCategory = functions.https.onCall(async (params, context) => {
 })
 
 exports.removeCategory = functions.https.onCall(async (params, context) => {
-    if (context.auth == null) throw new HttpsError('permission-denied', 'User is not Authenticated!!')
+    const admin = require('firebase-admin')
+    if (!is_removeCategory_initialized) {
+        admin.initializeApp()
+        is_removeCategory_initialized = true
+    }
+    if (context.auth == null) throw new functions.https.HttpsError('permission-denied', 'User is not Authenticated!!')
     const userID = context.auth.uid
     console.log(`userID ${userID} invoked removeCategory`)
     const categoryName = params.categoryName
@@ -91,20 +110,25 @@ exports.removeCategory = functions.https.onCall(async (params, context) => {
     const customClaims = userRecord.customClaims
     console.log(`Custom claims of user ${userID} : ${JSON.stringify(customClaims)}`)
     if (customClaims == null || !customClaims['isModerator']) {
-        throw new HttpsError('permission-denied', 'Deletion not possible as user is not a moderator')
+        throw new functions.https.HttpsError('permission-denied', 'Deletion not possible as user is not a moderator')
     }
     const db = admin.firestore()
     return await db.runTransaction(async t => {
         const categoryRef = db.doc(`categories/${categoryName}`)
         const categoryDoc = await t.get(categoryRef)
         if (!categoryDoc.exists)
-            throw new HttpsError('unavailable', `Deletion not possible as category ${categoryName} does not exist`)
+            throw new functions.https.HttpsError('unavailable', `Deletion not possible as category ${categoryName} does not exist`)
         return t.delete(categoryRef)
     })
 })
 
 exports.upVote = functions.https.onCall(async (params, context) => {
-    if (context.auth == null) throw new HttpsError('permission-denied', 'User is not Authenticated!!')
+    const admin = require('firebase-admin')
+    if (!is_upVote_initialized) {
+        admin.initializeApp()
+        is_upVote_initialized = true
+    }
+    if (context.auth == null) throw new functions.https.HttpsError('permission-denied', 'User is not Authenticated!!')
     const userID = context.auth.uid
     console.log(`userID ${userID} invoked upVote`)
     const categoryName = params.categoryName
@@ -113,7 +137,7 @@ exports.upVote = functions.https.onCall(async (params, context) => {
         const categoryRef = db.doc(`categories/${categoryName}`)
         const categoryDoc = await t.get(categoryRef)
         if (!categoryDoc.exists)
-            throw new HttpsError('unavailable', `Upvote not possible as category ${categoryName} does not exist`)
+            throw new functions.https.HttpsError('unavailable', `Upvote not possible as category ${categoryName} does not exist`)
         return t.update(categoryRef, { 'count': categoryDoc.data()['count'] + 1 })
     })
 })
